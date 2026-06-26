8a5998df85b71e1c169b7e1271de492a611ce21e
YSHCNYWFH <githubyosh1@gmail.com>
2026-06-26 16:36:02 +0800

    test gitignore

--- app/core/Database.php
+++ app/core/Database.php
@@ -3,8 +3,8 @@ class Database {
     public static function connect() {
         return new PDO(
             "mysql:host=localhost;dbname=daltondb",
-            "",
-            "",
+            "phpmyadmin",
+            "pkii@1111",
             [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
         );
     }
